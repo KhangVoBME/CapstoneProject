ab2a85a76949f84e4920153c00e79a93c543ac83
odiyan-diyan <70537106+odiyan-diyan@users.noreply.github.com>
2024-04-03 11:06:20 -0700

    Update Main.ino

--- Main.ino
+++ Main.ino
@@ -90,4 +90,83 @@ void loop() {
   delay(250);
   }//else brace
 
-}//loop brace
+}
+/*
+one side connects to 3v
+other goes to 10k which goes to ground
+side not connected to 3v goes to a0 or any analog pin
+*/
+double normal = 20; //0-20% threshold for normal pressure
+double infection = 50; //21-50% threshold for risk of infection
+double tdamage = 70; // 51-70% threshold for risk of tissue damage
+int fsrPin = A0;
+double value = 0;
+double percent = 0;     // the FSR and 10K pulldown are connected to a0
+    // the analog reading from the FSR resistor divider
+
+unsigned long lastA = 0; //time from last alert
+const unsigned long interval = 21600000; //6 hour interval update
+ 
+void loop() {
+    unsigned long currentTime = millis();            //gets current time
+    value = analogRead(fsrPin); //reads pin    
+    percent = map(value, 0, 1023, 0 ,100); //way to change analog reading to percentage
+    
+
+        if(percent <= normal){ //if  percentage is in certain threshold then it will output the correct message
+        Serial.println("Normal Range");
+
+        }else if(percent <= infection && percent>= normal){ 
+        display.print("Risk of Infection");
+        display.print("Pressure off by");
+        display.print(percent-normal);
+        display.print("% at: ");
+        showTime(currentTime);
+
+        }else if(percent <= tdamage && percent>= infection){
+        display.print("Risk of Tissue Damage");
+        display.print("Pressure off by");
+        display.print(percent-normal);
+        display.print("% at: ");
+        showTime(currentTime);
+        }
+        if(currentTime  - lastA >= interval){ // checks if 6 hours has passed
+            lastA = currentTime;
+            sixH(currentTime); //calls to 6 hour threshold function
+        }
+       delay(100);
+     }
+
+void showTime(unsigned long currentTime) { // gets time to look right
+  unsigned long hours = currentTime / 3600000;
+  unsigned long minutes = (currentTime % 3600000) / 60000;
+  
+  if (hours < 10) {
+    display.print("0");
+  }
+  display.print(hours);
+  display.print(":");
+  if (minutes < 10) {
+    display.print("0");
+  }
+  display.print(minutes);
+}
+
+void sixH(unsigned long currentTime){ //function to check every 6 hours
+  if (percent <= normal) { 
+    display.print("Normal Range");
+  } else if (percent <= infection && percent >= normal) {
+    display.print("Risk of Infection");
+    Sdisplay.print("Pressure off by ");
+    display.print(percent-normal);
+    display.print("% at:");
+    showTime(currentTime);
+  } else if (percent <= tdamage && percent >= infection) {
+    display.print("Risk of Tissue Damage");
+    display.print("Pressure off by ");
+    display.print(percent-normal);
+    display.print("% at:");
+    showTime(currentTime);
+  }
+    delay(100);
+}
