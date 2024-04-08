dc6ffbdb7360c36e3c419be24c827e9d88d2c243
odiyan-diyan <70537106+odiyan-diyan@users.noreply.github.com>
2024-04-08 09:19:43 -0700

    Update FSR.ino

--- FSR.ino
+++ FSR.ino
@@ -20,6 +20,8 @@ unsigned long lastA = 0; //time from last alert
 const unsigned long interval = 21600000; //6 hour interval update
 
 void setup(void) {
+    pinMode(fsrPin, INPUT);
+    display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
   // We'll send debugging information via the Serial monitor
   Serial.begin(115200);   
 }
@@ -30,28 +32,29 @@ void loop() {
     percent = map(value, 0, 1023, 0 ,100); //way to change analog reading to percentage
     
 
-        if(percent <= normal){ //if  percentage is in certain threshold then it will output the correct message
-        Serial.println("Normal Range");
+       // if(percent <= normal){ //if  percentage is in certain threshold then it will output the correct message
+      //  display.println("Normal Range");
 
-        }else if(percent <= infection && percent>= normal){ 
-        Serial.println("Risk of Infection");
-        Serial.println("Pressure off by");
-        Serial.print(percent-normal);
-        Serial.print("% at: ");
+        if(percent <= infection && percent>= normal){ 
+          display.println("Risk of Infection");
+          display.println("Pressure off by");
+          display.print(percent-normal);
+          display.print("% at: ");
         showTime(currentTime);
 
         }else if(percent <= tdamage && percent>= infection){
-        Serial.println("Risk of Tissue Damage");
-        Serial.println("Pressure off by");
-        Serial.print(percent-normal);
-        Serial.print("% at: ");
+          display.println("Risk of Tissue Damage");
+          display.println("Pressure off by");
+          display.print(percent-normal);
+          display.print("% at: ");
         showTime(currentTime);
         }
         if(currentTime  - lastA >= interval){ // checks if 6 hours has passed
             lastA = currentTime;
             sixH(currentTime); //calls to 6 hour threshold function
         }
-       delay(100);
+        delay(1000);
+        display.display();
      }
 
 void showTime(unsigned long currentTime) { // gets time to look right
@@ -59,31 +62,32 @@ void showTime(unsigned long currentTime) { // gets time to look right
   unsigned long minutes = (currentTime % 3600000) / 60000;
   
   if (hours < 10) {
-    Serial.print("0");
+      display.print("0");
   }
-  Serial.print(hours);
-  Serial.print(":");
+    display.print(hours);
+    display.print(":");
   if (minutes < 10) {
-    Serial.print("0");
+      display.print("0");
   }
-  Serial.print(minutes);
+    display.print(minutes);
 }
 
 void sixH(unsigned long currentTime){ //function to check every 6 hours
   if (percent <= normal) { 
-    Serial.println("Normal Range");
+      display.println("Normal Range");
   } else if (percent <= infection && percent >= normal) {
-    Serial.println("Risk of Infection");
-    Serial.print("Pressure off by ");
-    Serial.print(percent-normal);
-    Serial.print("% at:");
+      display.println("Risk of Infection");
+      display.print("Pressure off by ");
+      display.print(percent-normal);
+      display.print("% at:");
     showTime(currentTime);
   } else if (percent <= tdamage && percent >= infection) {
-    Serial.println("Risk of Tissue Damage");
-    Serial.print("Pressure off by ");
-    Serial.print(percent-normal);
-    Serial.print("% at:");
+      display.println("Risk of Tissue Damage");
+      display.print("Pressure off by ");
+      display.print(percent-normal);
+      display.print("% at:");
     showTime(currentTime);
   }
-    delay(100);
+    delay(1000);
+    display.display();
 }
