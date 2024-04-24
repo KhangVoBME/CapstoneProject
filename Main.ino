0891fed71dbe827a9c6628d49943b1515d9924a4
odiyan-diyan <70537106+odiyan-diyan@users.noreply.github.com>
2024-04-23 20:53:42 -0700

    Update Main.ino

--- Main.ino
+++ Main.ino
@@ -1,172 +1,320 @@
-//Include up here
+// pretty sure most of the sd card code can be removed but after i copied and pasted a bunch of random stuff from the sd card file,
+// it worked so i didnt really want to take the risk of removing the random sd card code.
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SH110X.h>
+#include "RTClib.h"
 #include "Adafruit_SHT4x.h"
+#include "FS.h"
+#include "SD.h"
 
-//Change if order if needed
-const int button1 13 //Left
-const int button2 12 //Right
-const int button3 27 //Down
-const int button4 33 //Up
-const int button5 15 //Center
-
-const int buzzer = A12;
-const int forceSensor = A0;
-
+Adafruit_SH1107 display = Adafruit_SH1107(64, 128, &Wire);
 Adafruit_SHT4x sht4 = Adafruit_SHT4x();
+RTC_PCF8523 rtc;
 
+const int NUM_ITEMS = 6;
+const int MAX_ITEM_LENGTH = 20;
+const int BUTTON_UP_PIN = 15;
+const int BUTTON_SELECT_PIN = 32;
+const int BUTTON_DOWN_PIN = 27; //this should be 13. 27 is the left button pin actually.
+const int BUTTON_CENTER_PIN = 33; 
+const int fsrPin = A0;
+const int buzzerPin = A6;
+string patientName;
 
-void homeScreen(){
-  display.print("Home Screen!");
-  display.display();
-}//homeScreen brace
+char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+char menu_items[NUM_ITEMS][MAX_ITEM_LENGTH] = {
+    {"Alerts"},
+    {"Humidity"},
+    {"Temp"},
+    {"Pressure"},
+    {"Clock"},
+    {"Patient Info"}};
 
-void screenOff(){
-  display.clearDisplay();
-  display.display();
-}//screenOff brace
+struct Alert {
+  String timestamp;
+  String sensor;
+  String message;
+};
 
-void screenOn(){
+const int MAX_ALERTS = 10; // max number of alerts
+Alert alerts[MAX_ALERTS];   // array to store alerts
+int numAlerts = 0;           // current number of alerts
+int currentAlertIndex = 0;   // index of the current alert being displayed
 
-}//screenOn brace
+int item_selected = 0; //index  of current menu item selected
+int current_screen = 0; // 0 = Home screen, 1 = Menu screen, 2 = Data screen
+unsigned long lastBuzz = 0;
+const unsigned long buzzInterval = 10000; // 10 seconds
 
-void playBuzzer(int buzzerFreq){
-  pinMode(buzzer, OUTPUT);
-  tone(buzzer, buzzerFreq);
-  delay(1000);
-  noTone(buzzer);
-}//playBuzzer brace
-/*
-Use this line to play buzzer, value is the frequency used. We can have different sounds for different situations.
-playBuzzer(1000);
-*/
+void initRTC() {
+  Wire.begin();
+  if (!rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    while (1);
+  }
+
+  if (!rtc.initialized() || rtc.lostPower()) {
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  }
+  rtc.start();
+
+  
+   // Calibrate RTC
+  float drift = 43; // seconds plus or minus over observation period - set to 0 to cancel previous calibration.
+  float period_sec = (7 * 86400);  // total observation period in seconds (86400 = seconds in 1 day:  7 days = (7 * 86400) seconds )
+  float deviation_ppm = (drift / period_sec * 1000000); //  deviation in parts per million (μs)
+  float drift_unit = 4.34; // use with offset mode PCF8523_TwoHours
+
+  int offset = round(deviation_ppm / drift_unit);
+  rtc.adjust(DateTime(F(__DATE__), F(__TIME__)).unixtime() + offset);
+}
+
+String getDateTime() {
+  DateTime now = rtc.now();
+  String dateTimeString = "";
+  dateTimeString += String(now.day(), DEC) + '/';
+  dateTimeString += String(now.month(), DEC) + '/';
+  dateTimeString += String(now.year(), DEC) + " ";
+  if (now.hour() < 10) {
+    dateTimeString += "0";
+  }
+  dateTimeString += String(now.hour(), DEC) + ':';
+  dateTimeString += String(now.minute(), DEC) + ':';
+  dateTimeString += String(now.second(), DEC);
+  return dateTimeString;
+}
+// read temp/humidity values from sensor 
+void readSHT4x(float &temperature, float &humidity) {
+  sensors_event_t humidity_event, temperature_event;
+  sht4.getEvent(&humidity_event, &temperature_event);
+  temperature = temperature_event.temperature;
+  humidity = humidity_event.relative_humidity;
+}
+//read pressure 
+float readPressure() {
+  int value = analogRead(fsrPin) / 4095;
+  return value;
+}
+//this function is called if sensor condition is met
+void buzz(String sensor) {
+  // basically checks for 10 seconds between buzzes
+  unsigned long currentTime = millis();
+  if (currentTime - lastBuzz >= buzzInterval) {
+    pinMode(buzzerPin, OUTPUT);
+    tone(buzzerPin, 10000);
+    delay(1000);
+    noTone(buzzerPin);
+    lastBuzz = currentTime; //update the last buzz
+
+    // write the alerts into the sd card 
+    String filename = "/sensors.txt";
+    File file = SD.open(filename, FILE_APPEND); // set to only APPEND the file.
+    if (file) { // basically, there needs to be a file called sensors already. the code does not check/create a new file.
+      file.print(getDateTime());
+      file.print(" : \n"); 
+      file.println(sensor);
+      file.close();
+      Serial.println("alert to sd card ");
+    } else {
+      Serial.println("failed to open");
+    }
+
+    // add to alerts 
+    if (numAlerts < MAX_ALERTS) {
+      alerts[numAlerts].timestamp = getDateTime();
+      alerts[numAlerts].sensor = sensor;
+      alerts[numAlerts].message = "\nAlert triggered by:\n" + sensor;
+      numAlerts++;
+    }
+  }
+}
 
 void setup() {
-
-  pinMode(button1, INPUT);
-  pinMode(button2, INPUT);
-  pinMode(button3, INPUT);
-  pinMode(button4, INPUT);
-  pinMode(button5, INPUT);
   Serial.begin(115200);
-
-  delay(250);
-  display.begin(0x3C, true);
-  display.display();//splash screen
-  delay(1000);
-  display.clearDisplay();
-  display.display();
-  display.setRotation(1);
-  display.setTextSize(1);
+  initRTC();
+  display.begin();
   display.setTextColor(SH110X_WHITE);
-  display.setCursor(0,0);
+  display.setTextSize(2); 
+  display.setRotation(1);
+  pinMode(BUTTON_UP_PIN, INPUT_PULLUP);
+  pinMode(BUTTON_SELECT_PIN, INPUT_PULLUP);
+  pinMode(BUTTON_DOWN_PIN, INPUT_PULLUP);
+  pinMode(BUTTON_CENTER_PIN, INPUT_PULLUP); 
 
-  homeScreen();
+  if (!sht4.begin()) {
+    Serial.println("Couldn't find SHT4x");
+    while (1);
+  }
 
-}//setup brace
+  if(!SD.begin()){
+    Serial.println("Card Mount Failed");
+    return;
+  }
+  uint8_t cardType = SD.cardType();
 
-void loop() {
-  forceReading = analogRead(forceSensor);//Will constantly read force
-
-  if(digitalRead(button1) == LOW){
-  delay(250);
-  }//if brace
-
-  else if(digitalRead(button2) == LOW){
-  delay(250);
-  }//else if brace
-
-  else if(digitalRead(button3) == LOW){
-  delay(250);
-  }//else if brace
-
-  else if(digitalRead(button4) == LOW){
-  delay(250);
-  }//else if brace
-
-  else if(digitalRead(button5) == LOW){
-  delay(250);
-  }//else if brace
-
-  else{//this will be button 5
-  delay(250);
-  }//else brace
-
-}
-/*
-one side connects to 3v
-other goes to 10k which goes to ground
-side not connected to 3v goes to a0 or any analog pin
-*/
-double normal = 20; //0-20% threshold for normal pressure
-double infection = 50; //21-50% threshold for risk of infection
-double tdamage = 70; // 51-70% threshold for risk of tissue damage
-int fsrPin = A0;
-double value = 0;
-double percent = 0;     // the FSR and 10K pulldown are connected to a0
-    // the analog reading from the FSR resistor divider
-
-unsigned long lastA = 0; //time from last alert
-const unsigned long interval = 21600000; //6 hour interval update
- 
-void loop() {
-    unsigned long currentTime = millis();            //gets current time
-    value = analogRead(fsrPin); //reads pin    
-    percent = map(value, 0, 1023, 0 ,100); //way to change analog reading to percentage
-    
-
-        if(percent <= normal){ //if  percentage is in certain threshold then it will output the correct message
-        Serial.println("Normal Range");
-
-        }else if(percent <= infection && percent>= normal){ 
-        display.print("Risk of Infection");
-        display.print("Pressure off by");
-        display.print(percent-normal);
-        display.print("% at: ");
-        showTime(currentTime);
-
-        }else if(percent <= tdamage && percent>= infection){
-        display.print("Risk of Tissue Damage");
-        display.print("Pressure off by");
-        display.print(percent-normal);
-        display.print("% at: ");
-        showTime(currentTime);
-        }
-        if(currentTime  - lastA >= interval){ // checks if 6 hours has passed
-            lastA = currentTime;
-            sixH(currentTime); //calls to 6 hour threshold function
-        }
-       delay(100);
-     }
-
-void showTime(unsigned long currentTime) { // gets time to look right
-  unsigned long hours = currentTime / 3600000;
-  unsigned long minutes = (currentTime % 3600000) / 60000;
+  if(cardType == CARD_NONE){
+    Serial.println("No SD card attached");
+    return;
+  }
   
-  if (hours < 10) {
-    display.print("0");
+  Serial.print("SD Card Type: ");
+  if(cardType == CARD_MMC){
+    Serial.println("MMC");
+  } else if(cardType == CARD_SD){
+    Serial.println("SDSC");
+  } else if(cardType == CARD_SDHC){
+    Serial.println("SDHC");
+  } else {
+    Serial.println("UNKNOWN");
   }
-  display.print(hours);
-  display.print(":");
-  if (minutes < 10) {
-    display.print("0");
-  }
-  display.print(minutes);
+
+  uint64_t cardSize = SD.cardSize() / (1024 * 1024);
+  Serial.printf("SD Card Size: %lluMB\n", cardSize);
 }
 
-void sixH(unsigned long currentTime){ //function to check every 6 hours
-  if (percent <= normal) { 
-    display.print("Normal Range");
-  } else if (percent <= infection && percent >= normal) {
-    display.print("Risk of Infection");
-    Sdisplay.print("Pressure off by ");
-    display.print(percent-normal);
-    display.print("% at:");
-    showTime(currentTime);
-  } else if (percent <= tdamage && percent >= infection) {
-    display.print("Risk of Tissue Damage");
-    display.print("Pressure off by ");
-    display.print(percent-normal);
-    display.print("% at:");
-    showTime(currentTime);
+void displayHomeScreen() {
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  display.print("Name: ");
+  display.println(patientName);
+  display.print("Time: ");
+  display.println(getDateTime());
+  display.print("Alert Count: ");
+  display.println(numAlerts);
+  display.display();
+}
+
+void displayMenuScreen() {
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  for (int i = 0; i < NUM_ITEMS; i++) {
+    if (i == item_selected) {
+      display.println(">" + String(menu_items[i]));
+    } else {
+      display.println("  " + String(menu_items[i]));
+    }
   }
-    delay(100);
+  display.display();
+}
+
+void displayDataScreen() {
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  display.setTextSize(2);
+  display.setTextColor(SH110X_WHITE);
+  switch (item_selected) {
+    case 0: // Alerts
+      display.println("Alerts:");
+      for (int i = 0; i < numAlerts; i++) {
+        display.println(String(alerts[i].timestamp) + ": " + String(alerts[i].sensor));
+      }
+      break;
+    case 1: // Humidity
+      float humidity, temperature;
+      readSHT4x(temperature, humidity);
+      display.print("Humidity: ");
+      display.print(humidity);
+      display.println("%");
+      break;
+    case 2: // Temperature
+      readSHT4x(temperature, humidity);
+      display.print("Temperature: ");
+      display.print(temperature);
+      display.println(" C");
+      break;
+    case 3: // Pressure
+      display.print("Pressure: ");
+      display.println(readPressure());
+      break;
+    case 4: // Clock
+      display.println("Current Time:");
+      display.println(getDateTime());
+      break;
+    case 5: // Patient Info
+      File patientInfoFile = SD.open("/patientinfo.txt");
+      if (patientInfoFile) {
+          patientName = patientInfoFile.readStringUntil('\n');
+          String patientAge = patientInfoFile.readStringUntil('\n');
+          String patientWeight = patientInfoFile.readStringUntil('\n');
+          String patientWound = patientInfoFile.readStringUntil('\n');
+          
+          // Displaying patient information on the screen
+          display.println("Patient Info:");
+          display.println("Name: " + patientName);
+          display.println("Age: " + patientAge);
+          display.println("Weight: " + patientWeight);
+          display.println("Wound: " + patientWound);
+
+          patientInfoFile.close();
+      } else {
+          display.println("Failed to load info.");
+          Serial.println("Failed to open patientinfo.txt");
+      }
+      break;
+  }
+  display.display();
+}
+
+void loop() {
+  // Read sensors and check conditions for alerts
+  float pressure = readPressure();
+  float temperature, humidity;
+  readSHT4x(temperature, humidity);
+
+  if (pressure == 1 && humidity > 60 && temperature > 27.7) {
+    buzz("Pressure\nHumidity\nTemperature");
+  } else if ((temperature > 27.7 && humidity > 60) || (temperature > 27.7 && pressure == 1) || (pressure == 1 && humidity > 60)) {
+    if (temperature > 27.7 && humidity > 60) {
+      buzz("Humidity\nTemperature");
+    }
+    if (temperature > 27.7 && pressure == 1) {
+      buzz("Pressure\nTemperature");
+    }
+    if (pressure == 1 && humidity > 60) {
+      buzz("Pressure\nHumidity");
+    }
+  } else if (pressure == 1) {
+    buzz("Pressure");
+  } else if (humidity > 60) {
+    buzz("Humidity");
+  } else if (temperature > 27.7) {
+    buzz("Temperature");
+  }
+
+  // Screen navigation and display updates
+  if (digitalRead(BUTTON_SELECT_PIN) == LOW) {
+    delay(200);
+    current_screen = (current_screen + 1) % 3; // Cycle through screens
+  }
+
+  if (current_screen == 1) { // Menu Screen navigation
+    if (digitalRead(BUTTON_UP_PIN) == LOW) {
+      item_selected = (item_selected > 0) ? item_selected - 1 : NUM_ITEMS - 1;
+      delay(200);
+    }
+
+    if (digitalRead(BUTTON_DOWN_PIN) == LOW) {
+      item_selected = (item_selected < NUM_ITEMS - 1) ? item_selected + 1 : 0;
+      delay(200);
+    }
+  }
+ // function to clear alerts on Alerts screen when center is pressed
+  if (current_screen == 2 && item_selected == 0 && digitalRead(BUTTON_CENTER_PIN) == LOW) { 
+    numAlerts = 0; // Reset the number of alerts to 0
+    Serial.println("All alerts cleared.");
+    delay(200);
+  }
+  switch (current_screen) {
+    case 0:
+      displayHomeScreen();
+      break;
+    case 1:
+      displayMenuScreen();
+      break;
+    case 2:
+      displayDataScreen();
+      break;
+  }
 }
